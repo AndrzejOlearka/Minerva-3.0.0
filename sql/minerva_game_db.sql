d3194aa212661e59322d72020726bb11a17e4a7e
AndrzejOlearka <olearka1992@gmail.com>
2018-12-12 20:22:03 +0100

    adding updated database file

--- sql/minerva_game_db.sql
+++ sql/minerva_game_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 06 Lis 2018, 02:35
+-- Czas generowania: 12 Gru 2018, 20:21
 -- Wersja serwera: 10.1.36-MariaDB
 -- Wersja PHP: 7.2.11
 
@@ -19,8 +19,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `minervabase`
+-- Baza danych: `minerva_game_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `banned_users`
+--
+
+CREATE TABLE `banned_users` (
+  `user` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `banning_date` datetime NOT NULL,
+  `nick_change_required` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `banned_users`
+--
+
+INSERT INTO `banned_users` (`user`, `banning_date`, `nick_change_required`) VALUES
+('mariola', '2019-01-03 19:26:19', 1);
 
 -- --------------------------------------------------------
 
@@ -49,9 +68,11 @@ CREATE TABLE `basic_equipment` (
 --
 
 INSERT INTO `basic_equipment` (`user`, `ambers`, `agates`, `malachites`, `turquoises`, `amethysts`, `topazes`, `emeralds`, `rubies`, `sapphires`, `diamonds`, `silver`, `gold`) VALUES
-('adrian', 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+('admin', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+('adrian', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
 ('andus', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-('gosia', 42238, 4521, 0, 1143, 45, 0, 0, 0, 0, 0, 0, 0);
+('gosia', 91406, 21115, 10, 5386, 45, 0, 0, 0, 0, 0, 111, 2316),
+('mariola', 185, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -72,9 +93,11 @@ CREATE TABLE `expeditions_data` (
 --
 
 INSERT INTO `expeditions_data` (`user`, `expedition_number`, `expedition_prize`, `expedition_start`, `expedition_end`) VALUES
-('adrian', 0, 1, '2018-11-05 22:32:14', '2018-11-05 22:32:24'),
+('admin', 0, 0, '2018-11-23 22:20:30', '2018-11-23 22:20:30'),
+('adrian', 0, 0, '2018-12-02 10:05:38', '2018-12-02 10:05:38'),
 ('andus', 0, 0, '2018-11-05 22:07:53', '2018-11-05 22:07:53'),
-('gosia', 0, 0, '2018-11-06 00:03:04', '2018-11-06 00:03:10');
+('gosia', 0, 0, '2018-12-05 23:06:55', '2018-12-05 23:07:05'),
+('mariola', 0, 0, '2018-11-16 10:28:49', '2018-11-16 10:28:59');
 
 -- --------------------------------------------------------
 
@@ -97,7 +120,7 @@ CREATE TABLE `guilds_data` (
 --
 
 INSERT INTO `guilds_data` (`id_guild`, `guild_name`, `guild_leader`, `guild_description`, `guild_members`, `guild_exp`, `guild_avatar`) VALUES
-(4, 'Anduskowo', 5, 'Anduskowo to gildia dla fanÃ³w butÃ³w aaaaa', 1, 0, 'guild-avatar.JPG');
+(5, 'Anduskowo', 5, 'Anduskowo to gildia dla fanÃ³w butÃ³w aaaaa', 1, 1800, 'guild-avatar.JPG');
 
 -- --------------------------------------------------------
 
@@ -127,9 +150,29 @@ CREATE TABLE `mines_data` (
 --
 
 INSERT INTO `mines_data` (`user`, `mines_ambers`, `mines_agates`, `mines_malachites`, `mines_turquoises`, `mines_amethysts`, `mines_topazes`, `mines_emeralds`, `mines_rubies`, `mines_sapphires`, `mines_diamonds`, `mines_gold`, `mines_silver`, `daily_prize`) VALUES
-('adrian', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2018-11-05 22:31:12'),
+('admin', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2018-11-23 22:20:30'),
+('adrian', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2018-12-02 10:05:38'),
 ('andus', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2018-11-05 22:07:53'),
-('gosia', 1, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, '2018-11-05 20:00:06');
+('gosia', 6, 2, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, '2018-11-22 23:55:10'),
+('mariola', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2018-11-15 23:20:43');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `moderators`
+--
+
+CREATE TABLE `moderators` (
+  `user` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `advance_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `moderators`
+--
+
+INSERT INTO `moderators` (`user`, `advance_date`) VALUES
+('gosia', '2018-11-25 00:01:56');
 
 -- --------------------------------------------------------
 
@@ -155,9 +198,11 @@ CREATE TABLE `rare_equipment` (
 --
 
 INSERT INTO `rare_equipment` (`user`, `crystals`, `morganites`, `fluorites`, `painites`, `aquamarines`, `jadeites`, `cymophanes`, `opals`, `pearls`) VALUES
+('admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 ('adrian', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 ('andus', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('gosia', NULL, NULL, 9, NULL, 17, 7, NULL, NULL, NULL);
+('gosia', 450, NULL, 26, NULL, 17, 52, NULL, NULL, NULL),
+('mariola', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -178,9 +223,11 @@ CREATE TABLE `trips_data` (
 --
 
 INSERT INTO `trips_data` (`user`, `trip_number`, `trip_prize`, `trip_start`, `trip_end`) VALUES
-('adrian', 0, 0, '2018-11-05 22:31:12', '2018-11-05 22:31:12'),
+('admin', 0, 0, '2018-11-23 22:20:30', '2018-11-23 22:20:30'),
+('adrian', 0, 0, '2018-12-02 10:05:38', '2018-12-02 10:05:38'),
 ('andus', 0, 0, '2018-11-05 22:07:53', '2018-11-05 22:07:53'),
-('gosia', 0, 0, '2018-11-06 01:01:02', '2018-11-06 01:01:15');
+('gosia', 0, 0, '2018-12-05 22:59:01', '2018-12-05 22:59:21'),
+('mariola', 0, 0, '2018-11-16 09:43:14', '2018-11-16 09:43:34');
 
 -- --------------------------------------------------------
 
@@ -205,13 +252,21 @@ CREATE TABLE `user_data` (
 --
 
 INSERT INTO `user_data` (`id`, `user`, `password`, `email`, `level`, `exp`, `premium_end`, `coins`, `id_guild`) VALUES
-(5, 'gosia', '$2y$10$S9dy4finqnmxHrtgAzn0duLy5/MjeyIEnqcj.z7ZDIztv97OYkhaG', 'gosia@op.pl', 10, 5663011, '2018-11-07 12:17:08', 18576, 4),
+(5, 'gosia', '$2y$10$S9dy4finqnmxHrtgAzn0duLy5/MjeyIEnqcj.z7ZDIztv97OYkhaG', 'gosia@op.pl', 10, 12954161, '2018-11-25 19:01:29', 46521, 5),
 (6, 'andus', '$2y$10$1ag0xfUOqkFloV4kRyc9BOscg0EhwAJT31iyVitaJsp7atVZg/HiG', 'andus@op.pl', 1, 1, '2018-11-05 22:07:53', 5, 0),
-(7, 'adrian', '$2y$10$elwnPwlqULhUNr98rBhgQ.8o7H9bUXKO7W0yE/3JgweUhX/80iZsG', 'adrian@op.pl', 1, 201, '2018-11-05 22:31:11', 3, 0);
+(8, 'mariola', '$2y$10$tgKOG3Va6nbGDwKDqNsIzucxAILM.Pc3pXvQvh4iOwxjX0XeiShxS', 'maria@op.pl', 2, 1199, '2018-11-18 23:20:43', 993, 0),
+(9, 'admin', '$2y$10$5WyGDcTvaverKGK.B49AwOWHSJ4G5pSpaOPj4GualrqhhjX6wGXPW', 'admin@gmail.com', 1, 1, '2018-11-26 22:20:30', 5, 0),
+(10, 'adrian', '$2y$10$c.SFU9OrkFmh7DW7KeacWudVGZoBlTMP7MRAzYNSafbB9TfK3Xy4C', 'adrian@op.pl', 1, 1, '2018-12-05 10:05:38', 5, 0);
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `banned_users`
+--
+ALTER TABLE `banned_users`
+  ADD PRIMARY KEY (`user`);
 
 --
 -- Indeksy dla tabeli `basic_equipment`
@@ -235,6 +290,12 @@ ALTER TABLE `guilds_data`
 -- Indeksy dla tabeli `mines_data`
 --
 ALTER TABLE `mines_data`
+  ADD PRIMARY KEY (`user`);
+
+--
+-- Indeksy dla tabeli `moderators`
+--
+ALTER TABLE `moderators`
   ADD PRIMARY KEY (`user`);
 
 --
@@ -263,13 +324,13 @@ ALTER TABLE `user_data`
 -- AUTO_INCREMENT dla tabeli `guilds_data`
 --
 ALTER TABLE `guilds_data`
-  MODIFY `id_guild` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_guild` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT dla tabeli `user_data`
 --
 ALTER TABLE `user_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
